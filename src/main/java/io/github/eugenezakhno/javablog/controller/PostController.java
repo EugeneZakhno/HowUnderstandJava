9bc668089352d2df9babf640d8364557428bc081
EugeneZakhno <zohan1234567@gmail.com>
2020-07-28 22:55:04 +0300

    deleted @ResponseBody

--- src/main/java/io/github/eugenezakhno/javablog/controller/PostController.java
+++ src/main/java/io/github/eugenezakhno/javablog/controller/PostController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class PostController {
 
     @GetMapping("/")
-    @ResponseBody
     public String hello (){
         return "hello";
     }
