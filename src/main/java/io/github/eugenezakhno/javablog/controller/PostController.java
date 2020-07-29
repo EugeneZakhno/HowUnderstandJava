76f0454271d8701ab81627d1f7ca5e321b9f9415
EugeneZakhno <zohan1234567@gmail.com>
2020-07-29 16:34:27 +0300

    added link https://www.youtube.com/watch?v=OUreWcL6zcc

--- src/main/java/io/github/eugenezakhno/javablog/controller/PostController.java
+++ src/main/java/io/github/eugenezakhno/javablog/controller/PostController.java
@@ -3,7 +3,7 @@ package io.github.eugenezakhno.javablog.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+// https://www.youtube.com/watch?v=OUreWcL6zcc
 @Controller
 public class PostController {
 
